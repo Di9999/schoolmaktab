29944a560e8fd75b0c722dec6b2081d1831e4b31
Di9999 <83602755+Di9999@users.noreply.github.com>
2021-11-04 13:30:30 +0500

    ddddd

--- school-two/src/pages/Boshsahifa.js
+++ school-two/src/pages/Boshsahifa.js
@@ -1,14 +1,46 @@
-
-
 import React ,{Component} from 'react'
-
-
+import style from "../css/Boshsahifa.module.css";
+import { Container, Navbar, Row, Nav, } from "react-bootstrap";
 
 export default class BoshSahifa extends Component{
     render(){
         return(
             <div>
-Hello
+<>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  <br />
+  <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+</>
             </div>
         )
     }
