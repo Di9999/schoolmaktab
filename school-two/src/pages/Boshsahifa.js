fd80b8f1f47a7c1d64badbcb97d5bd2748461349
Di9999 <83602755+Di9999@users.noreply.github.com>
2021-11-04 13:04:35 +0500

    aaaaaaa

---
+++ school-two/src/pages/Boshsahifa.js
@@ -0,0 +1,15 @@
+
+
+import React ,{Component} from 'react'
+
+
+
+export default class BoshSahifa extends Component{
+    render(){
+        return(
+            <div>
+Hello
+            </div>
+        )
+    }
+}
