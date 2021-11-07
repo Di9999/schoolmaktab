c471345ac926a4e84f6ea3e45b8dd5159c689ab9
Di9999 <83602755+Di9999@users.noreply.github.com>
2021-11-07 20:46:34 +0500

    ddddd

--- school-two/src/pages/Boshsahifa.js
+++ school-two/src/pages/Boshsahifa.js
@@ -3,6 +3,9 @@ import style from "../css/Boshsahifa.module.css";
 import { Col, Container, Card, Row, Image } from "react-bootstrap";
 import registr from "../img/registr.jpg";
 import Navbar from "./Navbar";
+import "react-multi-carousel/lib/styles.css";
+import { Clusterer, Map, Placemark, YMaps } from "react-yandex-maps";
+import Carousel from "react-multi-carousel";
 export default class Boshsahifa extends Component {
   render() {
     return (
@@ -21,7 +24,6 @@ export default class Boshsahifa extends Component {
             </Card>
           </div>
         </Container>
-
         <Container>
           <Row className={`${style.qabul} `}>
             <div>
@@ -57,7 +59,6 @@ export default class Boshsahifa extends Component {
             </div>
           </Row>
         </Container>
-
         <Container>
           <Row className={`${style.orange} `}>
             <div>
@@ -93,12 +94,11 @@ export default class Boshsahifa extends Component {
             </div>
           </Row>
         </Container>
-
         <div className={style.cardinfo}>
           <Row>
             <Col lg={12}>
-              <h1 className={style.sarlavha}>Biz haqimizda</h1>
               <br />
+              <h1 className={style.sarlavha1}>Biz haqimizda</h1>
               <br />
             </Col>
             <Col lg={4} md={6} sm={12} className={style.cardBolimlar}>
@@ -120,11 +120,11 @@ export default class Boshsahifa extends Component {
 
                 <h1>Sport mashg'ulotlari</h1>
                 <p>
-                  Maktabimiz oshxonasi hozirda rekanstruksiya qilinyapti. Yangi
-                  o'quv yilida o'quvchilarimizga to'liq jihozlangan, gigiyena
-                  qoidalariga rioya qilingan va mahoratli oshpazlarimiz
-                  tomonidan pishirilgan turli xil mazali taomlar bilan
-                  boyitilgan oshxonani taqdim etamiz.
+                  Maktabimizda barcha sprot anjomlari bilan jihozlangan, yetarli
+                  kattalikdagi maydonga ega sport zalimiz mavjud. Bu sprot zalda
+                  bolalarimiz jismoniy tarbiyao'tish bilan bir qatorda darsdan
+                  bo'sh vaqtlarida turli sport turlari bilan shug'ullanishlari
+                  mumkin.
                 </p>
               </div>
             </Col>
@@ -139,11 +139,11 @@ export default class Boshsahifa extends Component {
                 <img />
                 <h1>San'at va madaniyat</h1>
                 <p>
-                  Maktabimiz oshxonasi hozirda rekanstruksiya qilinyapti. Yangi
-                  o'quv yilida o'quvchilarimizga to'liq jihozlangan, gigiyena
-                  qoidalariga rioya qilingan va mahoratli oshpazlarimiz
-                  tomonidan pishirilgan turli xil mazali taomlar bilan
-                  boyitilgan oshxonani taqdim etamiz.
+                  Maktabimizda musiqa sohasida yetuk tajribaga ega o'qituvchimiz
+                  va koplab musiqa asboblari mavjud. Musiqa o'qituvchimiz
+                  boshchiligida maktabimiz yoshlari san'atning har xil turlari
+                  bo'yicha turli ko'rik tanlovlarda yuqori natijalarni egallab
+                  kelishyapti
                 </p>
               </div>
             </Col>
@@ -152,11 +152,10 @@ export default class Boshsahifa extends Component {
                 <img />
                 <h1>Axborot texnologiyalari</h1>
                 <p>
-                  Maktabimiz oshxonasi hozirda rekanstruksiya qilinyapti. Yangi
-                  o'quv yilida o'quvchilarimizga to'liq jihozlangan, gigiyena
-                  qoidalariga rioya qilingan va mahoratli oshpazlarimiz
-                  tomonidan pishirilgan turli xil mazali taomlar bilan
-                  boyitilgan oshxonani taqdim etamiz.
+                  Maktabimizda kompyuter va boshqa axborot texnologiyalari bilan
+                  jihozlangan informatika xonalarimiz mavjud. Bu xonalarda
+                  bolalarimiz dasturlashning turli sohalari bilan shug'ullanishi
+                  mumkin
                 </p>
               </div>
             </Col>
@@ -166,11 +165,10 @@ export default class Boshsahifa extends Component {
 
                 <h1>Xavfsizlik va qo'riqlash</h1>
                 <p>
-                  Maktabimiz oshxonasi hozirda rekanstruksiya qilinyapti. Yangi
-                  o'quv yilida o'quvchilarimizga to'liq jihozlangan, gigiyena
-                  qoidalariga rioya qilingan va mahoratli oshpazlarimiz
-                  tomonidan pishirilgan turli xil mazali taomlar bilan
-                  boyitilgan oshxonani taqdim etamiz.
+                  Maktabimizda boralarimizning xavfsizligini ta'minlash
+                  maqsadida maktabning kirish qismida qorovullarimiz va
+                  navbatchi o'qituvchilarimiz tomonidan maktabimizga kelayotgan
+                  har bir inson suhbatdan o'tqaziladi va ro'yhatga olinadi.
                 </p>
               </div>
             </Col>
@@ -192,17 +190,148 @@ export default class Boshsahifa extends Component {
                 >
                   <h1>Sog'lik va salomatlik</h1>
                   <p>
-                    Maktabimiz oshxonasi hozirda rekanstruksiya qilinyapti.
-                    Yangi o'quv yilida o'quvchilarimizga to'liq jihozlangan,
-                    gigiyena qoidalariga rioya qilingan va mahoratli
-                    oshpazlarimiz tomonidan pishirilgan turli xil mazali taomlar
-                    bilan boyitilgan oshxonani taqdim etamiz.
+                    Maktabimizda bolalarimiz salomatligini saqlash maqsadida
+                    barcha asbob-anjomlar bilan jihozlangan tibbiyot xonalarimiz
+                    va tibbiyot sohasida yuqori tajribaga ega hamshiralarimiz
+                    mavjud
                   </p>
                 </div>
               </div>
             </Col>
           </Row>
         </div>
+        <div className="sliderHomiy" style={{ backgroundColor: "white" }}>
+          {/* <Carousel
+            responsive={responsive1}
+            autoPlay={this.props.deviceType !== "mobile" ? true : false}
+            autoPlaySpeed={3000}
+            infinite={true}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://president.uz/oz" target="_blank">
+                </a>
+              </div>
+              <p>O'zbekiston Respublikasi Prezidentining rasmiy veb-sayti</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://www.gov.uz/uz" target="_blank">
+                </a>
+              </div>
+              <p>O'zbekiston Respublikasining Hukumat portali</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://lex.uz/" target="_blank">
+                </a>
+              </div>
+              <p>O'zbekiston Respublikasi qonun hujjatlari milliy bazasi</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://my.gov.uz/oz" target="_blank">
+                </a>
+              </div>
+              <p>Interaktiv davlat xizmatlarining Yagona portali</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://www.uzedu.uz/" target="_blank">
+                </a>
+              </div>
+              <p>O'zbekiston Respublikasi xalq ta'limi vazirligi</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "20px",
+                alingItems: "center",
+              }}
+            >
+              <div className="toolpat" style={{ backgroundColor: "white" }}>
+                <a href="https://ittower.uz/" target="_blank">
+                </a>
+              </div>
+              <p>IT Tower firmasi </p>
+            </div>
+          </Carousel> */}
+        </div>
+        <div className={style.xarita}>
+          <br />
+          <h1 className={style.sarlavha}> Bizning manzilimiz</h1>
+          <br />
+
+          <YMaps>
+            <div className={style.manzilmap}>
+              <Map
+                style={{
+                  width: "100%",
+                  height: "400px",
+                  border: "4px solid white",
+                }}
+                defaultState={{ center: [41.3572425, 60.818505], zoom: 12 }}
+              >
+                <Clusterer
+                  options={{
+                    groupByCoordinates: false,
+                  }}
+                >
+                  <Placemark
+                    key={-1}
+                    geometry={[41.3572425, 60.818505]}
+                    options={{
+                      iconLayout: "default#image",
+                    }}
+                    properties={{
+                      hintContent: `<h6><b className="personStyle">1 - maktab</b></h6>`,
+                    }}
+                    modules={["geoObject.addon.hint"]}
+                  />
+                </Clusterer>
+              </Map>
+              <br />
+              <br />
+            </div>
+          </YMaps>
+        </div>
+        <br /> <br />
       </div>
     );
   }
